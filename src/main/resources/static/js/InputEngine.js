71d660e6e6ca821970263cc41c45a208ec650d44
vl4dkravets <vl4d.kravets@gmail.com>
2021-06-02 14:29:45 -0400

    repitions just once - keydown - without skipping - testing

--- src/main/resources/static/js/InputEngine.js
+++ src/main/resources/static/js/InputEngine.js
@@ -36,7 +36,7 @@ InputEngine.prototype.setupBindings = function() {
         40 : this.onKeyDown
     }, 1);
 
-    document.addEventListener('keypress', this.onKeyDown);
+    document.addEventListener('keydown', this.onKeyDown);
     document.addEventListener('keyup', this.onKeyUp);
 };
 
