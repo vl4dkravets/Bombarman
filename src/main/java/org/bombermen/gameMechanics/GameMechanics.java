e120a090b0217971b14eee9ab796d2b314441b6c
vl4dkravets <vl4d.kravets@gmail.com>
2021-06-07 09:50:14 -0400

    removed code for testing/checking; ready for prod

--- src/main/java/org/bombermen/gameMechanics/GameMechanics.java
+++ src/main/java/org/bombermen/gameMechanics/GameMechanics.java
@@ -37,7 +37,6 @@ public class GameMechanics implements Tickable, Comparable {
         walls = new ArrayList<>();
         woods = new ArrayList<>();
         bombs = new ArrayList<>();
-        //fires = new ArrayList<>();
         firesDefaultPositions = new ArrayList<>();
         firesLeft = new ArrayList<>();
         destroyedWoods = new ArrayList<>();
@@ -47,6 +46,7 @@ public class GameMechanics implements Tickable, Comparable {
 
         createWallsAndWoods();
         createPawnsAndBombs();
+
         replica.writeReplicaToInitializeGameField(pawns, bombs, woods, walls, Topic.START);
     }
 
@@ -104,7 +104,8 @@ public class GameMechanics implements Tickable, Comparable {
         Pawn pawn1 = pawns.get(0);
         Pawn pawn2 = pawns.get(1);
         pawn1.setPosition(TILE_SIZE, TILE_SIZE);
-        pawn2.setPosition(TILE_SIZE, TILE_SIZE*2);
+        //pawn2.setPosition(TILE_SIZE, TILE_SIZE*2);
+        pawn2.setPosition(GAME_FIELD_W - TILE_SIZE, GAME_FIELD_H - TILE_SIZE);
         pawn1.setBomb(new Bomb(pawn1.getPosition()));
         pawn2.setBomb(new Bomb(pawn2.getPosition()));
         bombs.add(pawn1.getBomb());
@@ -295,8 +296,6 @@ public class GameMechanics implements Tickable, Comparable {
         Iterator<Bomb> iterator = bombs.iterator();
         while(iterator.hasNext()) {
             Bomb bomb = iterator.next();
-
-            if(bombs.indexOf(bomb) == 0) continue;
 
             if (bomb.updateBombTimerAndCheck(elapsed)) {
                 // System.out.println(bomb + " HAS EXPLODED!!");
