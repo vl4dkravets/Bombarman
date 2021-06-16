a103a0ec786c0e74b353479ea08a8cb50ee81456
vl4dkravets <vl4d.kravets@gmail.com>
2021-06-16 19:32:08 -0400

    test class for Message class

--- src/test/java/org/bombermen/message/MessageTest.java
+++ src/test/java/org/bombermen/message/MessageTest.java
@@ -2,15 +2,64 @@ package org.bombermen.message;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonbTester;
+import org.springframework.core.io.Resource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringJUnitConfig
+@JsonTest
 class MessageTest {
+    private Message message;
+    private Topic topic;
 
-    @BeforeEach
-    void setUp() {
+    @Autowired
+    private JacksonTester<Message> jsonTest;
+
+    @Value("classpath:moveMessage.json")
+    private Resource moveMessage;
+    @Value("classpath:plantBombMessage.json")
+    private Resource plantBombMessage;
+
+
+    @Test
+    void checkState() {
+        Topic topic = Topic.MOVE;
+        String data = "{\"direction\":\"LEFT\"}";
+        String playerName = "player0";
+        message = new Message(topic, data);
+        message.setPlayerName(playerName);
+        assertEquals(topic, message.getTopic());
+        assertEquals(data, message.getData());
+        assertEquals(playerName, message.getPlayerName());
     }
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    void deserializeMoveMessageTest() throws IOException {
+        final Message message = jsonTest.readObject(moveMessage);
+        assertAll(
+                () -> assertNotNull(message),
+                () -> assertEquals(Topic.MOVE, message.getTopic()),
+                () -> assertEquals("{\"direction\":\"DOWN\"}", message.getData())
+                );
     }
+
+    @Test
+    void deserializePlantBombMessageTest() throws IOException {
+        final Message message = jsonTest.readObject(plantBombMessage);
+        assertAll(
+                () -> assertNotNull(message),
+                () -> assertEquals(Topic.PLANT_BOMB, message.getTopic()),
+                () -> assertEquals("{}", message.getData())
+        );
+    }
+
 }
