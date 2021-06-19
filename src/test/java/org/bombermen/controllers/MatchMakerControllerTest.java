198d432411818f3a77bdd129d5e2a99239f407dc
vl4dkravets <vl4d.kravets@gmail.com>
2021-06-19 10:44:50 -0400

    test class for MatchMakerController

--- src/test/java/org/bombermen/controllers/MatchMakerControllerTest.java
+++ src/test/java/org/bombermen/controllers/MatchMakerControllerTest.java
@@ -1,21 +1,51 @@
 package org.bombermen.controllers;
 
+import org.bombermen.services.GameService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@WebMvcTest(MatchMakerController.class)
 class MatchMakerControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    private GameService gameService;
 
     @BeforeEach
     void setUp() {
+        gameService = GameService.getInstance();
     }
 
     @AfterEach
     void tearDown() {
+        gameService = null;
     }
 
     @Test
-    void getGameId() {
+    void getGameId() throws Exception {
+        String url = "/matchmaker/join";
+        String param = "name=";
+        String attribute = "test123";
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post(url)
+                .accept(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .content(param+attribute)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+        String outputInJson = response.getContentAsString();
+        assertEquals(attribute, outputInJson);
     }
 }
